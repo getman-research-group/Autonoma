7dbde9dca7aa422360a4cb796e34d11932621f91
Thomas Delvaux <Exper1mental@users.noreply.github.com>
2022-04-11 12:39:23 -0400

    Create UserVariables.sh
    Added missing file containing user variables.

---
+++ Scripts/UserVariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+task_type="Gaussian"
+# Acceptable values:
+#   "Gaussian"
+#   "VASP"
+
+max_runs=5
+# The maximum number of times Autonoma
+# will try to rerun a script before stopping.
